421491cab4c8810f8522ac535f2ec1905c7b8001
Chris Dostert <chris.dostert@live.com>
2018-10-08 16:44:45 -0700

    feat) initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+[![Build Status](https://travis-ci.org/opspec-pkgs/gcp.gke.create.svg?branch=master)](https://travis-ci.org/opspec-pkgs/gcp.gke.create)
+
+# Problem statement
+
+creates a kubernetes cluster in google kubernetes engine service (if it doesn't exist)
+
+# Format
+
+the op uses [![opspec 0.1.6](https://img.shields.io/badge/opspec-0.1.6-brightgreen.svg?colorA=6b6b6b&colorB=fc16be)](https://opspec.io/0.1.6) definition format
+
+# Example usage
+
+## Install
+
+```shell
+opctl op install github.com/opspec-pkgs/gcp.gke.create#1.0.0
+```
+
+## Run
+
+```
+opctl run github.com/opspec-pkgs/gcp.gke.create#1.0.0
+```
+
+## Compose
+
+```yaml
+op:
+  ref: github.com/opspec-pkgs/gcp.gke.create#1.0.0
+  inputs:
+    keyFile:
+    name:
+    projectId:
+    # params w/ default
+    diskSize:
+    diskType:
+    machineType:
+    masterAuthorizedNetworks:
+    maxNodes:
+    minNodes:
+    network:
+    nodeLocations:
+    numNodes:
+    region:
+    subnetwork:
+    zone:
+```
+
+# Support
+
+join us on
+[![Slack](https://opspec-slackin.herokuapp.com/badge.svg)](https://opspec-slackin.herokuapp.com/)
+or
+[open an issue](https://github.com/opspec-pkgs/gcp.gke.create/issues)
+
+# Releases
+
+releases are versioned according to
+[![semver 2.0.0](https://img.shields.io/badge/semver-2.0.0-brightgreen.svg)](http://semver.org/spec/v2.0.0.html)
+and [tagged](https://git-scm.com/book/en/v2/Git-Basics-Tagging); see
+[CHANGELOG.md](CHANGELOG.md) for release notes
+
+# Contributing
+
+see
+[project/CONTRIBUTING.md](https://github.com/opspec-pkgs/project/blob/master/CONTRIBUTING.md)
---
+++ cmd.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+set -e
+
+gcloud auth activate-service-account --key-file=/keyFile
+
+echo "checking for exiting gke cluster"
+clusterDescribeCmd="gcloud container clusters describe $name --project $projectId"
+
+# handle opts
+if [ "$zone" != " " ]; then
+  clusterDescribeCmd=$(printf "%s --zone %s" "$clusterDescribeCmd" "$zone")
+fi
+if [ "$region" != " " ]; then
+  clusterDescribeCmd=$(printf "%s --region %s" "$clusterDescribeCmd" "$region")
+fi
+
+if eval "$clusterDescribeCmd" >/dev/null 2>&1
+then
+  echo "found exiting gke cluster"
+  exit
+else
+  echo "existing gke cluster not found"
+fi
+
+echo "creating gke cluster..."
+clusterCreateCmd="gcloud container clusters create $name"
+clusterCreateCmd=$(printf "%s --project %s" "$clusterCreateCmd" "$projectId")
+clusterCreateCmd=$(printf "%s --disk-size %s" "$clusterCreateCmd" "$diskSize")
+clusterCreateCmd=$(printf "%s --disk-type %s" "$clusterCreateCmd" "$diskType")
+clusterCreateCmd=$(printf "%s --network %s" "$clusterCreateCmd" "$network")
+clusterCreateCmd=$(printf "%s --num-nodes %s" "$clusterCreateCmd" "$numNodes")
+clusterCreateCmd=$(printf "%s --machine-type %s" "$clusterCreateCmd" "$machineType")
+clusterCreateCmd=$(printf "%s --max-nodes %s" "$clusterCreateCmd" "$maxNodes")
+clusterCreateCmd=$(printf "%s --min-nodes %s" "$clusterCreateCmd" "$minNodes")
+
+# handle opts
+if [ "$async" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --async" "$clusterCreateCmd")
+fi
+if [ "$enableAutoscaling" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --enable-autoscaling" "$clusterCreateCmd")
+fi
+if [ "$enableAutorepair" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --enable-autorepair" "$clusterCreateCmd")
+else
+  clusterCreateCmd=$(printf "%s --no-enable-autorepair" "$clusterCreateCmd")
+fi
+if [ "$enableAutoupgrade" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --enable-autoupgrade" "$clusterCreateCmd")
+fi
+if [ "$enableCloudLogging" = "false" ]; then
+  clusterCreateCmd=$(printf "%s --no-enable-cloud-logging" "$clusterCreateCmd")
+fi
+if [ "$enableCloudMonitoring" = "false" ]; then
+  clusterCreateCmd=$(printf "%s --no-enable-cloud-monitoring" "$clusterCreateCmd")
+fi
+if [ "$enableIpAlias" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --enable-ip-alias" "$clusterCreateCmd")
+fi
+if [ "$enableNetworkPolicy" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --enable-network-policy" "$clusterCreateCmd")
+fi
+if [ "$enableMasterAuthorizedNetworks" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --enable-master-authorized-networks" "$clusterCreateCmd")
+fi
+if [ "$enablePrivateEndpoint" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --enable-private-endpoint" "$clusterCreateCmd")
+fi
+if [ "$enablePrivateNodes" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --enable-private-nodes" "$clusterCreateCmd")
+fi
+if [ "$masterAuthorizedNetworks" != " " ]; then
+  clusterCreateCmd=$(printf "%s --master-authorized-networks %s" "$clusterCreateCmd" "$masterAuthorizedNetworks")
+fi
+if [ "$nodeLocations" != " " ]; then
+  clusterCreateCmd=$(printf "%s --node-locations %s" "$clusterCreateCmd" "$nodeLocations")
+fi
+if [ "$preemptible" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --preemptible" "$clusterCreateCmd")
+fi
+if [ "$region" != " " ]; then
+  clusterCreateCmd=$(printf "%s --region %s" "$clusterCreateCmd" "$region")
+fi
+if [ "$subnetwork" != " " ]; then
+  clusterCreateCmd=$(printf "%s --subnetwork %s" "$clusterCreateCmd" "$subnetwork")
+fi
+if [ "$zone" != " " ]; then
+  clusterCreateCmd=$(printf "%s --zone %s" "$clusterCreateCmd" "$zone")
+fi
+
+eval "$clusterCreateCmd"
